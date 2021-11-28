04bdfb5e6391562d724d1624d03af976d67e2183
Denis Z <ziulev@pm.me>
2021-11-28 19:37:55 +0100

    Configure indent_blankline

--- nvim/init.lua
+++ nvim/init.lua
@@ -378,6 +378,13 @@ vim.opt.listchars:append("space:⋅")
 require("indent_blankline").setup {
     show_end_of_line = false,
     space_char_blankline = " ",
+    buftype_exclude = {"terminal"},
+    filetype_exclude = {"dashboard", "NvimTree", "packer"},
+    show_current_context = true,
+    context_patterns = {
+        "class", "return", "function", "method", "^if", "^while", "jsx_element", "^for", "^object", "^table", "block", "arguments", "if_statement",
+        "else_clause", "jsx_element", "jsx_self_closing_element", "try_statement", "catch_clause", "import_statement", "operation_type"
+    }
 }
 
 
